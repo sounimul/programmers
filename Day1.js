d199b086102550eeeab7f5abedc9fbf04f0fc490
Gyeong <sounimul12@gmail.com>
2023-01-01 22:58:01 +0900

    second commit

---
+++ Day1.js
@@ -0,0 +1,17 @@
+/*
+Day 1 사칙연산
+*/
+
+//두 수의 합
+function solution(num1, num2) {
+    return num1+num2;
+}
+
+//두 수의 차
+const solution = (num1, num2) => num1 - num2;
+
+//두 수의 곱
+const solution = (num1, num2) => num1*num2;
+
+//몫 구하기
+const solution = (num1, num2) => parseInt(num1/num2);
